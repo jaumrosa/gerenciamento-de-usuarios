54a593c657871a0ebba6e98b89c3beba65c21e30
jaumrosa <joao.rosa5@outlook.com>
2026-05-03 19:32:01 -0300

    G8 - Captura do submit do formulário com preventDefault() e geração do objeto user

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let fields = document.querySelectorAll("#form-user-create [name]");
 let user = {};
 
-
-fields.forEach((field, index) => {
-  (field.name === "gender" && field.checked) ? user[field.name] = field.value : user[field.name] = field.value;   
-  }
-);
-
-console.log(user);
+document.getElementById("form-user-create").addEventListener("submit", (event) => {
+  event.preventDefault();
+  fields.forEach((field, index) => {
+    (field.name === "gender" && field.checked) ? user[field.name] = field.value : user[field.name] = field.value;   
+  });
+  console.log(user);
+});
