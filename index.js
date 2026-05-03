9318d494e7c06793e7c336e3723488aba88e16e4
João Pedro Rosa <joao.rosa5@outlook.com>
2026-05-02 21:10:08 -0300

    G6 - Implementação da captura do gênero selecionado no formulário

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let fields = document.querySelectorAll("#form-user-create [name]");
 fields.forEach((field, index) => {
-  console.log(field.name);
-});
+  (field.name === "gender" && field.checked) ? console.log(field, "Esse é o gênero selecionado no input") : ""   
+  }
+);
