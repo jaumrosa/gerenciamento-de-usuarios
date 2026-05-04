ee0a67edb24087d9b02b655edce084a5b22f463a
jaumrosa <joao.rosa5@outlook.com>
2026-05-03 21:09:17 -0300

    G9 - Adição dinâmica de usuários na tabela a partir dos dados do formulário

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 let fields = document.querySelectorAll("#form-user-create [name]");
 let user = {};
 
-document.getElementById("form-user-create").addEventListener("submit", (event) => {
+function addLine(dataUser){
+  let tr = document.createElement("tr");
+  tr.innerHTML = `
+      <tr>
+        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+        <td>${dataUser.name}</td>
+        <td>${dataUser.email}</td>
+        <td>${dataUser.admin}</td>
+        <td>${dataUser.birth}</td>
+        <td>
+          <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+        </td>
+      </tr>
+ `;
+  document.querySelector("#table-users").appendChild(tr);
+}
+
+document.querySelector("#form-user-create").addEventListener("submit", (event) => {
   event.preventDefault();
   fields.forEach((field, index) => {
     (field.name === "gender" && field.checked) ? user[field.name] = field.value : user[field.name] = field.value;   
   });
-  console.log(user);
+  addLine(user);
 });
