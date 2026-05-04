1cc98fb8724646982603c46916ccdd1530b1e9a3
jaumrosa <joao.rosa5@outlook.com>
2026-05-03 22:13:18 -0300

    G13 - Implementação de upload de foto com FileReader e callback para exibição na tabela

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -8,8 +8,27 @@ class UserController{
     onSubmit(){
         this.formEl.addEventListener("submit", event => {
             event.preventDefault();
-            this.addLine(this.getValues());
-    })};
+            let values = this.getValues();
+            this.getPhoto((content) => {
+                values.photo = content;
+                this.addLine(values);
+            });
+        });
+    }
+
+    getPhoto(callback){
+        let fileReader = new FileReader();
+        let elements = [...this.formEl.elements].filter(item => item.name === 'photo');
+        let file = elements[0].files[0];
+
+        fileReader.onload = () => {
+            callback(fileReader.result);
+
+        }
+
+        fileReader.readAsDataURL(file);
+
+    }
 
     getValues(){
         let user = {};
@@ -22,7 +41,7 @@ class UserController{
     addLine(dataUser){
         this.tableEl.innerHTML += `
             <tr>
-                <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
                 <td>${dataUser.admin}</td>
