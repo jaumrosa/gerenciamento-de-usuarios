e2e67a329070daa60ff1fbbb3b6031e24fe14f12
jaumrosa <joao.rosa5@outlook.com>
2026-05-04 20:44:31 -0300

    G21 - Implementação da abertura do painel de edição de usuário

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,6 +3,13 @@ class UserController{
         this.formEl = document.querySelector(formId);
         this.tableEl = document.querySelector(tableId);
         this.onSubmit();
+        this.onEdit();
+    }
+
+    onEdit(){
+        document.querySelector("#box-user-update .btn-cancel").addEventListener("click", e=>{
+            this.showPanelCreate();
+        });
     }
 
     onSubmit(){
@@ -64,12 +71,26 @@ class UserController{
             <td>${(dataUser.admin)? 'Sim' : 'Não'}</td>
             <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
-                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
         `;
+        tr.querySelector(".btn-edit").addEventListener("click", e=>{
+            JSON.parse(tr.dataset.user);
+            this.showPanelUpdate();
+        });
         this.tableEl.appendChild(tr);
         this.updateCount();
+    }
+
+    showPanelCreate(){
+        document.querySelector("#box-user-update").style.display = "none";
+        document.querySelector("#box-user-create").style.display = "block";
+    }
+
+    showPanelUpdate(){
+        document.querySelector("#box-user-create").style.display = "none";
+        document.querySelector("#box-user-update").style.display = "block";
     }
 
     updateCount(){
@@ -79,11 +100,8 @@ class UserController{
             numberUsers++;
             let user = JSON.parse(tr.dataset.user);
             if (user._admin) numberAdmin++;
-
         });
-
         document.querySelector("#number-users").innerHTML = numberUsers;
         document.querySelector("#number-users-admin").innerHTML = numberAdmin;
-
     }
 }
