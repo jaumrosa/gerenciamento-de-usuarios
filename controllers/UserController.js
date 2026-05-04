d7458ba041e5f6860d7e700a98a55e04509ef5d7
jaumrosa <joao.rosa5@outlook.com>
2026-05-03 22:27:27 -0300

    G14 - Refatoração do upload de foto para uso de Promise com FileReader

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -9,25 +9,24 @@ class UserController{
         this.formEl.addEventListener("submit", event => {
             event.preventDefault();
             let values = this.getValues();
-            this.getPhoto((content) => {
+            this.getPhoto().then((content) =>{
                 values.photo = content;
                 this.addLine(values);
+            }, (e) =>{
+                console.error(e)
             });
         });
     }
 
-    getPhoto(callback){
-        let fileReader = new FileReader();
-        let elements = [...this.formEl.elements].filter(item => item.name === 'photo');
-        let file = elements[0].files[0];
-
-        fileReader.onload = () => {
-            callback(fileReader.result);
-
-        }
-
-        fileReader.readAsDataURL(file);
-
+    getPhoto(){
+        return new Promise((resolve, reject)=>{
+            let fileReader = new FileReader();
+            let elements = [...this.formEl.elements].filter(item => item.name === 'photo');
+            let file = elements[0].files[0];
+            fileReader.onload = () => {resolve(fileReader.result);}
+            fileReader.onerror = (e) => {reject(e);}
+            fileReader.readAsDataURL(file);
+        });
     }
 
     getValues(){
@@ -50,5 +49,5 @@ class UserController{
                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                 </td>
             </tr>`;
-}
+    }
 }
